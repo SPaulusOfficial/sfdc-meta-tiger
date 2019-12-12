453a63d8dbeaa38ba962aec1f1a60ac289f2e716
leonardo.carvalho <leonardo.carvalho.ext@ipiranga.ipiranga>
2019-12-12 12:19:51 -0300

    MetaGroup Fix

--- src/Metadata/metaGroup.cs
+++ src/Metadata/metaGroup.cs
@@ -4,9 +4,9 @@ using System.Xml.Serialization;
 using MetaTiger.ManageFile;
 
 namespace MetaTiger.Metadata{
-    class metaGroup : MetaBase {
+    class MetaGroup : MetaBase {
 
-		public metaGroup(){
+		public MetaGroup(){
 			this.m_list = new List<String>();
 			this.m_metaname = MetaConstants.Group;
 		}		
